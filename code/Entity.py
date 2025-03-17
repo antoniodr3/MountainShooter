a7ce32e4acf50c6b9e5860907779fc172b11e614
Tonny <ff7tonny2020@gmail.com>
2025-03-17 11:14:07 -0300

    Changes on Menu, background

--- code/Entity.py
+++ code/Entity.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
+from abc import ABC, abstractmethod
 
-class Entity:
-    pass
+import pygame
+
+
+class Entity(ABC):
+    def __init__(self, name: str, position: tuple):
+        # Obtendo o caminho absoluto do diretório atual do arquivo
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        # Corrigindo o caminho para o diretório de assets
+        asset_path = os.path.join(base_dir, '../asset/', name + '.png')
+
+        self.name = name
+        try:
+            # Tentando carregar a imagem usando o caminho absoluto
+            self.surf = pygame.image.load(asset_path)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Arquivo de imagem não encontrado: {asset_path}")
+
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.speed = 0
+
+    @abstractmethod
+    def move(self, ):
+        pass
--- code/Game.py
+++ code/Game.py
@@ -4,7 +4,8 @@ import os
 
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 from code.Menu import Menu
 
 
@@ -16,8 +17,17 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()
+                quit()
+            else:
+                pass
+
 
 
 
