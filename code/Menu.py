e496a3b126408eec43da1ac29f20f69903fa7216
Tonny <ff7tonny2020@gmail.com>
2025-03-17 08:28:07 -0300

    Creating asset folder insert all assets creating the menu: background, sound and fonts Creating the cinst file

--- code/Menu.py
+++ code/Menu.py
@@ -6,6 +6,8 @@ import os
 from pygame import Surface, Rect
 from pygame.font import Font
 
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
 
 class Menu:
     def __init__(self, window):
@@ -19,6 +21,13 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50,'*** Mountain', COLOR_ORANGE , ((WIN_WIDTH /2), 70))
+            self.menu_text(50, 'Shooter ***', COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+
             pygame.display.flip()
 
             # Check for all events
@@ -27,11 +36,13 @@ class Menu:
                     pygame.quit()  # Close Window
                     quit()  # end pygame
 
-    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple, text_rect=None):
-        text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
-        Text_rect: Rect = text_surf.get_rect(text_center_pos)
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
+
+
 
 
 
